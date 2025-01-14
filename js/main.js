da63d241a81fdf845fba46bb5a25142a9de6d2b3
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-01-13 22:18:55 -0600

    Started creating drop-down form and javascript click event
    Started making the drop-down form with all the options for cities and the framework for the click event when a user clicks "Submit".

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function (event) {
+    // Checking if the button was clicked
+    if (!event.target.matches("#button")) return;
+
+    var product = "civil"
+    var output = "json"
+    var lon = 50
+    var lat = 50
+
+    fetch("http://www.7timer.info/bin/api.pl?lon={\"lon\"}&lat={\"lat\"}&product={\"product\"}&output={\"output\"}")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+});
