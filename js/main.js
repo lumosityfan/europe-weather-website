7934b1b2ce9ad3289c1dee1a824e21fa8f226ca3
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-01-24 10:18:35 -0600

    Added cities in dropdown menu and added API connection code and started function to render Data, including sections for each day

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+const { renderToReadableStream } = require("react-dom/server");
+
 document.addEventListener("click", function (event) {
     // Checking if the button was clicked
-    if (!event.target.matches("#button")) return;
+    if (!event.target.matches("#weather")) return;
 
     var product = "civil"
     var output = "json"
     var lon = 50
     var lat = 50
 
-    fetch("http://www.7timer.info/bin/api.pl?lon={\"lon\"}&lat={\"lat\"}&product={\"product\"}&output={\"output\"}")
+    fetch(`http://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=${product}&output=${output}`)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => renderData(data))
+      .catch(() => console.log("Error"));
 });
+
+function renderData(data) {
+  
+}
